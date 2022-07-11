0b3a032db2f15dde29f333fa277c678bb41ddbca
HyeoncheolLee <hyunchal1098@gmail.com>
2022-07-12 01:57:40 +0900

    수정 및 삭제

--- src/App.js
+++ src/App.js
@@ -58,6 +58,29 @@ function Create(props) {
   </article>
 }
 
+function Update(props){
+  const [title, setTitle] = useState(props.title);  //입력박스 입력이 되지않아 state를 생성하여 입력한다. 
+  const [body, setBody] = useState(props.body);
+
+  return <article>
+  <h2>Update</h2>
+  <form onSubmit={event=>{
+    event.preventDefault(); //reload 막기위해
+    const title = event.target.title.value; //event.target는 해당 이벤트가 발생한 지점을 가리키므로 해당 태그를 가리키게 된다. !!
+    const body = event.target.body.value;
+    props.onUpdate(title,body);
+  }}>
+    <p><input type='text' name='title' placeholder='title' value={title} onChange={event=>{
+      setTitle(event.target.value); //onChange는 마지막 한글자만 적용이된다. 따라서 글자가 추가될때마다 State를 다시 저장한다.
+    }}/></p> 
+    <textarea name='body' placeholder='body' value={body} onChange={event=>{
+      setBody(event.target.value);
+    }}></textarea>
+    <p><input type='submit' value='Update'></input></p>
+  </form>
+</article>
+}
+
 function App() {
   // const _mode = useState('WELCOME');  //useState인자는 초기값을 받고 배열을 리턴한다. 1번째 배열은 'WELCOME' 2번째 배열은 함수를 리턴한다.
   // const mode = _mode[0]; //상태값은 0번째 
@@ -65,33 +88,62 @@ function App() {
   const [mode, setMode] = useState('WELCOME');  //위의 세줄 코드랑 같음
   const [_id, setId] = useState(null);
   const [nextId, setNextId] = useState(4); //현재  topoics에 id가 3까지있으므로 4를 주는것임 다른 의미는 없음
-  const [topics, setTopics] = useState([{id:1, title:"html", body:"html is ..." }
+  const [topics, setTopics] = useState([{id:1, title:"html", body:"html is ..." } //데이터집합 그냥 더미데이터임
                 , {id:2, title:"css", body:"css is ..." }
                 , {id:3, title:"javscript", body:"javscript is ..." }]);
   let content = null;
+  let contextContent = null;
   let _title = null;
   let _body = null;
   if(mode === 'WELCOME'){
     content = <Article title = 'WELCOME' body='hello, web...'></Article>
   }else if(mode === 'READ'){
+    console.log(topics, _id)
     
     _title = topics.find(x => x.id === Number(_id)).title;
     _body = topics.find(x => x.id === Number(_id)).body;
 
     content = <Article title = {_title} body={_body}></Article>
+    //react에서는 하나의 태그안에 html을 작성해야한다. 그러기 위하여 <></> 이렇게 빈태그(아무의미가 없는태그)를 넣어서 태그 두개를 이어붙힐수 있다.!!!!!
+    contextContent = <><li> 
+    <a href={'/update/' + _id} onClick={event=>{  //클릭한경우 발생하는 함수이므로 Update 버튼만 생김 클릭하는 순간 setMode가 실행
+    event.preventDefault();
+    setMode('UPDATE');
+  }}>Update</a> 
+  </li>
+  <li> 
+    <a onClick={()=>{  //클릭한경우 발생하는 함수이므로 Update 버튼만 생김 클릭하는 순간 setMode가 실행
+      const newTopics = [...topics];
+      newTopics.splice(newTopics[newTopics.findIndex(x=>x.id === Number(_id))],1);
+      console.log(newTopics);
+      setTopics(newTopics);
+      setMode('WELCOME');
+  }}>Delete</a> 
+  </li>
+  </>
   }else if(mode === 'CREATE'){
     content = <Create onCreate={(_title, _body)=>{
-      console.log(topics);
       const newTopic = {id:nextId, title:_title, body:_body}
       const newTopics = [...topics]; //topics 복제
       newTopics.push(newTopic);
       setTopics(newTopics);
-      console.log(topics);
       setMode('READ');
       setId(nextId);
       setNextId(nextId+1);
       
     }}></Create>
+  }else if(mode === 'UPDATE'){
+    let title1, body1 = null;
+    title1 = topics.find(x => x.id === Number(_id)).title;  //Array 함수 find()에서 x는 해당 배열중 하나의 값을 의미한다. find 함수 검색!!
+    body1 = topics.find(x => x.id === Number(_id)).body;
+    content = <Update title={title1} body={body1} onUpdate = {(title, body)=>{  //props로 title 와 body와 onUpdate를 받아온다.
+      const newTopics = [...topics];
+      const updateTopic = {id:Number(_id), title:title, body:body}; //===으로 비교를 하기때문에 자료형또한 맞춰줘야한다. 
+
+      newTopics[newTopics.findIndex(x=>x.id === Number(_id))] = updateTopic;
+      setTopics(newTopics);
+      setMode('READ');
+    }}></Update>
   }
 
   return (
@@ -110,14 +162,16 @@ function App() {
         setId(id);
       }}></Nav>
       {content}
-      <a href='/create' onClick={event=>{
-        event.preventDefault();
-        setMode('CREATE');
-      }}>Create</a>
-      <a href='/update' onClick={event=>{
-        event.preventDefault();
-        setMode('UPDATE');
-      }}>Update</a>
+      <ul>
+        <li>
+          <a href='/create' onClick={event=>{
+          event.preventDefault();
+          setMode('CREATE');
+        }}>Create</a>
+        </li>
+        {contextContent}
+      </ul>
+
       
     </div>
     
